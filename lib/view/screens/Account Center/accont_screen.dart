c98e447c3c77f70931c7a11ea57855ce0fd9c7a8
Shafqat05 <100747209+Shafqat05@users.noreply.github.com>
2024-01-17 15:49:52 +0500

    wedd

--- lib/view/screens/Account Center/accont_screen.dart
+++ lib/view/screens/Account Center/accont_screen.dart
@@ -11,6 +11,8 @@ class AccountCenterScreen extends StatefulWidget {
 }
 
 class _AccountCenterScreenState extends State<AccountCenterScreen> {
+  int index = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -145,126 +147,150 @@ class _AccountCenterScreenState extends State<AccountCenterScreen> {
                       padding:  EdgeInsets.symmetric(horizontal: 4.w),
                       child: Row(
                         children: [
-                          Container(
-                            height: 20.h,
-                            width: 28.w,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                border: Border.all(width: 1,color: Colors.grey.shade300)
-                            ),
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'FREE',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Color(0xfff97a2a),
-                                        fontSize: 15.sp
+                          GestureDetector(
+                            onTap: () {
+                              index= 1;
+                              setState(() {
+
+                              });
+                            },
+                            child: Container(
+                              height: 20.h,
+                              width: 28.w,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(width:index==1?2: 1,color: index==1 ? Color(0xfff97a2a) : Colors.grey.shade300)
+                              ),
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      'FREE',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Color(0xfff97a2a),
+                                          fontSize: 15.sp
+                                      ),
                                     ),
-                                  ),
-                                  SizedBox(height: 1.h,),
-                                  Text(
-                                    '3-days trail',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Color(0xff005e56).withAlpha(200),
-                                        //Color(0xff7b7b7b),
-                                        fontSize: 12.sp
+                                    SizedBox(height: 1.h,),
+                                    Text(
+                                      '3-days trail',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Color(0xff005e56).withAlpha(200),
+                                          //Color(0xff7b7b7b),
+                                          fontSize: 12.sp
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
                             ),
                           ),
                           SizedBox(width: 4.w,),
-                          Container(
-                            height: 20.h,
-                            width: 28.w,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                border: Border.all(width: 1,color: Colors.grey.shade300)
-                            ),
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'Rs 850.00',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Color(0xfff97a2a),
-                                        fontSize: 15.sp
+                          GestureDetector(
+                            onTap: () {
+                              index= 2;
+                              setState(() {
+
+                              });
+                            },
+                            child: Container(
+                              height: 20.h,
+                              width: 28.w,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(width:index==2?2: 1,color: index==2 ? Color(0xfff97a2a) : Colors.grey.shade300)
+                              ),
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      'Rs 850.00',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Color(0xfff97a2a),
+                                          fontSize: 15.sp
+                                      ),
                                     ),
-                                  ),
-                                  SizedBox(height: 1.h,),
-                                  Text(
-                                    '\$5.99',
-                                    style: TextStyle(
-                                        decoration: TextDecoration.lineThrough,
-                                        //fontWeight: FontWeight.bold,
-                                        color: Color(0xff005e56).withAlpha(130),
-                                        //Color(0xff7b7b7b),
-                                        fontSize: 9.sp
+                                    SizedBox(height: 1.h,),
+                                    Text(
+                                      '\$5.99',
+                                      style: TextStyle(
+                                          decoration: TextDecoration.lineThrough,
+                                          //fontWeight: FontWeight.bold,
+                                          color: Color(0xff005e56).withAlpha(130),
+                                          //Color(0xff7b7b7b),
+                                          fontSize: 9.sp
+                                      ),
                                     ),
-                                  ),
-                                  SizedBox(height: 1.h,),
-                                  Text(
-                                    'Quarterly',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Color(0xff005e56).withAlpha(200),
-                                        //Color(0xff7b7b7b),
-                                        fontSize: 12.sp
+                                    SizedBox(height: 1.h,),
+                                    Text(
+                                      'Quarterly',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Color(0xff005e56).withAlpha(200),
+                                          //Color(0xff7b7b7b),
+                                          fontSize: 12.sp
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
                             ),
                           ),
                           SizedBox(width: 4.w,),
-                          Container(
-                            height: 20.h,
-                            width: 28.w,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                border: Border.all(width: 1,color: Colors.grey.shade300)
-                            ),
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'Rs 850.00',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Color(0xfff97a2a),
-                                        fontSize: 15.sp
+                          GestureDetector(
+                            onTap: () {
+                              index= 3;
+                              setState(() {
+
+                              });
+                            },
+                            child: Container(
+                              height: 20.h,
+                              width: 28.w,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(width:index==3?2: 1,color: index==3 ? Color(0xfff97a2a) : Colors.grey.shade300)
+                              ),
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      'Rs 850.00',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Color(0xfff97a2a),
+                                          fontSize: 15.sp
+                                      ),
                                     ),
-                                  ),
-                                  SizedBox(height: 1.h,),
-                                  Text(
-                                    '\$23.99',
-                                    style: TextStyle(
-                                        decoration: TextDecoration.lineThrough,
-                                        //fontWeight: FontWeight.bold,
-                                        color: Color(0xff005e56).withAlpha(130),
-                                        //Color(0xff7b7b7b),
-                                        fontSize: 9.sp
+                                    SizedBox(height: 1.h,),
+                                    Text(
+                                      '\$23.99',
+                                      style: TextStyle(
+                                          decoration: TextDecoration.lineThrough,
+                                          //fontWeight: FontWeight.bold,
+                                          color: Color(0xff005e56).withAlpha(130),
+                                          //Color(0xff7b7b7b),
+                                          fontSize: 9.sp
+                                      ),
                                     ),
-                                  ),
-                                  SizedBox(height: 1.h,),
-                                  Text(
-                                    'Yearly',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Color(0xff005e56).withAlpha(200),
-                                        //Color(0xff7b7b7b),
-                                        fontSize: 12.sp
+                                    SizedBox(height: 1.h,),
+                                    Text(
+                                      'Yearly',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Color(0xff005e56).withAlpha(200),
+                                          //Color(0xff7b7b7b),
+                                          fontSize: 12.sp
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
                             ),
                           )
